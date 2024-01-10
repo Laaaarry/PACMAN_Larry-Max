6ee1c0a08f42cce124e30e828ff614ad468d2603
Laaaarry <lawrenceding49@gmail.com>
2024-01-10 12:00:59 -0500

    Added stuff
    base animation ready

--- Main.java
+++ Main.java
@@ -2,19 +2,25 @@ import javax.swing.*;
 
 // class that contains the frame and main method that runs the program
 public class Main extends JFrame{
-    private int frameHeight=600;
-    private int frameWidth=1000;
+    private int frameHeight;
+    private int frameWidth;
     public static void main(String[]args){
         new Main();
     }
     public Main(){
         Pacman pac=new Pacman();
         //System.out.println(pac.getHeight()+" "+pac.getWidth());
+        frameHeight=pac.getHeight();
+        frameWidth=pac.getWidth();
+        // Basic frame attributes
         this.setTitle("PACMAN");
         this.setSize(frameWidth,frameHeight);
         this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        // Adding pacman
         this.add(pac);
+        
         this.setVisible(true);
     }
 }
--- Pacman.java
+++ Pacman.java
@@ -3,11 +3,27 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Pacman extends JPanel implements ActionListener, KeyListener{
-    public Pacman(){
+    // instance variables
+    Timer timer = new Timer(1000, this);
 
+    boolean GameRunning = false;
+    boolean GameOver = false;
+    boolean GamePaused = true;
+    boolean InMenu = true;
+
+    // Constructor
+    public Pacman(){
+        addKeyListener(this);
+        setSize(1000,600);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(true);
+        this.setFocusable(true);
+        timer.start();
     }
     // Game Cycle
-
+    public void GameCycle(){
+        System.out.print("cycle\t");
+    }
     // Paint methods
 
     // Game States
@@ -16,10 +32,24 @@ public class Pacman extends JPanel implements ActionListener, KeyListener{
 
     // Event Listeners
     public void actionPerformed(ActionEvent e){
-
+        if(GameRunning){
+            GameCycle();
+        }
     }
     public void keyPressed(KeyEvent e){
-
+        int keyCode=e.getKeyCode();
+        if(keyCode==KeyEvent.VK_ENTER && !GameRunning){
+            GameRunning=true;
+            GamePaused=false;
+            InMenu=false;
+            GameOver=false;
+        }
+        if(keyCode==KeyEvent.VK_ESCAPE && GameRunning){
+            GameRunning=false;
+            GamePaused=true;
+            InMenu=true;
+            GameOver=false;
+        }
     }
     public void keyReleased(KeyEvent e){
 
