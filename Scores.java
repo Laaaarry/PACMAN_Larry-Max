65117a9de3e5931b88b9759bf78f8acddcf761dd
This-is-a-Usermane <156102346+This-is-a-Usermane@users.noreply.github.com>
2024-01-14 21:36:05 -0500

    SCORES! :D

--- Scores.java
+++ Scores.java
@@ -1,3 +1,26 @@
-public class Scores {
-
+import java.awt.*;
+public class Scores 
+{
+    private Pacman p;
+    private int highScore=0;
+    
+    public Scores(Pacman p)
+    {
+        this.p=p;
+    }
+    
+    //setting scoreboard
+    public void setScore(Graphics g)
+    {
+        p.GameText("Score: "+p.score,0,600,Color.WHITE,15,g);
+    }
+    
+    public void setHighScore(Graphics g)
+    {
+        if(p.score>highScore)
+        {
+            highScore=p.score;
+        }
+            p.GameText("High Score: "+highScore,100,600,Color.WHITE,15,g);
+    }
 }
